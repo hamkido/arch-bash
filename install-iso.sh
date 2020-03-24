9b82ee6b1bbb91011dad7c5aca92564655f6d4b6
hamki <hamki.do2000@gmail.com>
2020-03-24 19:20:23 +0800

    normal

--- install-iso.sh
+++ install-iso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ddpacman -S --noconfirm dialog || { echo "Error at script start: Are you sure you're running this as the root user? Are you sure you have an internet connection?"; exit; }
-dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "This is an Arch install script that is very rough around the edges.\n\nOnly run this script if you're a big-brane who doesn't mind deleting your entire /dev/sda drive.\n\nThis script is only really for me so I can autoinstall Arch.\n\nt. Luke"  15 60 || exit
-dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "Do you think I'm meming? Only select yes to DELET your entire /dev/sda and reinstall Arch.\n\nTo stop this script, press no."  10 60 || exit
+pacman -S --noconfirm dialog || { echo "Error at script start: Are you sure you're running this as the root user? Are you sure you have an internet connection?"; exit; }
+dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "This is an Arch install script that is very rough around the edges.\n\nOnly run this script if you're a big-brane who doesn't mind deleting your entire /dev/nvme0n1p drive.\n\nThis script is only really for me so I can autoinstall Arch.\n\nt. Luke"  15 60 || exit
+dialog --defaultno --title "DON'T BE A BRAINLET!" --yesno "Do you think I'm meming? Only select yes to DELET your entire /dev/nvme0n1p and reinstall Arch.\n\nTo stop this script, press no."  10 60 || exit
 dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> comp
 dialog --no-cancel --inputbox "Enter partitionsize in gb, separated by space (swap & root)." 10 60 2>psize
 IFS=' ' read -ra SIZE <<< $(cat psize)
@@ -10,7 +10,7 @@ if ! [ ${#SIZE[@]} -eq 2 ] || ! [[ ${SIZE[0]} =~ $re ]] || ! [[ ${SIZE[1]} =~ $r
     SIZE=(12 1000);
 fi
 timedatectl set-ntp true
-cat <<EOF | fdisk /dev/sda
+cat <<EOF | fdisk /dev/nvme0n1p
 o
 n
 p
@@ -26,16 +26,16 @@ p
 w
 EOF
 partprobe
-yes | mkfs.ext4 /dev/sda4
-yes | mkfs.ext4 /dev/sda3
-yes | mkfs.ext4 /dev/sda1
-mkswap /dev/sda2
-swapon /dev/sda2
-mount /dev/sda3 /mnt
+yes | mkfs.ext4 /dev/nvme0n1p4
+yes | mkfs.ext4 /dev/nvme0n1p3
+yes | mkfs.ext4 /dev/nvme0n1p1
+mkswap /dev/nvme0n1p2
+swapon /dev/nvme0n1p2
+mount /dev/nvme0n1p3 /mnt
 mkdir -p /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
 mkdir -p /mnt/home
-mount /dev/sda4 /mnt/home
+mount /dev/nvme0n1p4 /mnt/home
 pacstrap /mnt base base-devel linux linux-firmware networkmanager vim
 genfstab -U /mnt > /mnt/etc/fstab
 ### BEGIN
@@ -47,7 +47,7 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 systemctl enable NetworkManager
 systemctl start NetworkManager
-pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/nvme0n1p && grub-mkconfig -o /boot/grub/grub.cfg
 ### END
 mv comp /mnt/etc/hostname
 dialog --defaultno --title "Final Qs" --yesno "Reboot computer?"  5 30 && reboot
